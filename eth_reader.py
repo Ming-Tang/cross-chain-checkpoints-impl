41bb5c9ed1e47c73f3c1d731c39abbbe9f9cd08d
Ming-Tang <project.shinkirou@gmail.com>
2018-07-22 20:27:00 -0700

    Refactor eth_reader.py

--- eth_reader.py
+++ eth_reader.py
@@ -16,108 +16,157 @@ prefix = 'CpM1'
 hex_prefix = '0x' + codecs.encode(prefix.encode('ascii'), 'hex').decode('ascii')
 #print(hex_prefix)
 
-data = {'height': cons.main_height_init, 'transactions': {}, 'blocks': {}}
 
-try:
-    with open('data.json', 'r') as f:
-        data = json.load(f)
+data = None
 
-    copyfile('data.json', 'data.backup.json')
 
-except FileNotFoundError:
-    pass
+def find_initial_height(h0):
+    global data
+    while True:
+        last_hash = data['blocks'][str(h0)]['hash']
+        next_block = w3.eth.getBlock(h0 + 1)
+        if not next_block:
+            break
+        parent_hash = next_block.parentHash.hex()
+        if parent_hash != last_hash:
+            h0 -= 1
+        else:
+            break
+
+    return h0
+
+
+def import_block(i, last_hash):
+    global data
+    key = str(i)
+    block = w3.eth.getBlock(i)
+    print(block.hash.hex(), block.parentHash.hex() == last_hash)
+    last_hash = block.hash.hex()
+
+    data['blocks'][key] = {
+        'timestamp': block.timestamp,
+        'hash': block.hash.hex(),
+        'parentHash': block.parentHash.hex()
+    }
+
+    data['transactions'][key] = []
+
+    for txid in block.transactions:
+        tx = w3.eth.getTransaction(txid)
+        if tx.to == cons.cp_address and tx.input.find(hex_prefix) == 0:
+            tx_data = {
+                'blockHash': tx.blockHash.hex(),
+                'transactionIndex': tx.transactionIndex,
+                'hash': tx.hash.hex(),
+                'blockNumber': tx.blockNumber,
+                'from': tx['from'],
+                'input': tx.input
+            }
+            #print(tx.input.strip('0x'))
+
+            data['transactions'][key].append(tx_data)
+
+    return last_hash
+
 
 def sync():
     global data
     new_height = w3.eth.blockNumber
-
-    h0 = data['height'] - 5 # look 5 blocks behind in case of reorgs
+    h0 = find_initial_height(data['height'])
     print('Blocks to sync: {}'.format(new_height - h0))
+
     if new_height > h0:
+        last_hash = data['blocks'][str(h0 - 1)]['hash']
 
         for i in range(h0, new_height + 1):
-            key = str(i)
-            block = w3.eth.getBlock(i)
-            data['blocks'][key] = {
-                'timestamp': block.timestamp,
-                'hash': block.hash.hex(),
-                'parentHash': block.parentHash.hex()
-            }
-
-            data['transactions'][key] = []
             if i % 25 == 0:
                 print('{:.4}% {}'.format(
                     100.0 * (i - h0) / (new_height - h0), i))
 
-            blk = w3.eth.getBlock(i)
-            for txid in blk.transactions:
-                tx = w3.eth.getTransaction(txid)
-                if tx.to == cons.cp_address and tx.input.find(hex_prefix) == 0:
-                    tx_data = {
-                        'blockHash': tx.blockHash.hex(),
-                        'transactionIndex': tx.transactionIndex,
-                        'hash': tx.hash.hex(),
-                        'blockNumber': tx.blockNumber,
-                        'from': tx['from'],
-                        'input': tx.input
-                    }
-                    #print(tx.input.strip('0x'))
+            last_hash = import_block(i, last_hash)
 
-                    data['transactions'][key].append(tx_data)
 
     data['height'] = new_height
 
 
-sync()
-#print(data)
-#print(json.dumps(data, indent=2))
-with open('data.json', 'w') as f:
-    json.dump(data, f, indent=2, sort_keys=True)
+def import_tx(tx):
+    global data
+    tx_data = tx['input']
+    data_stripped = tx_data[len(hex_prefix):]
+    height = int('0x' + data_stripped[:6], 16)
+    header = data_stripped[6:]
+    decoded_header = deserialize_header(bfh(header), height=height)
+    verified = is_verified_header(header, bits=decoded_header['bits'])
+    order = (tx['blockNumber'], tx['transactionIndex'])
+    bhash = block_hash(header)
+
+    #print('{}:{:3} | {:7} {}'.format(order[0], order[1], height, header))
+    if not verified:
+        print('  *** Proof of work is not verified.')
+        return False
+
+    if existing_headers.get(header, None) not in (None, height):
+        print('  *** Header already exists in another height: ', existing_headers.get(header))
+        return False
+
+    existing_headers[header] = height
+
+    if cons.is_eligible_sub_height(height):
+        entry = CpEntry(
+            order,
+            data['blocks'][str(tx['blockNumber'])]['timestamp'],
+            decoded_header['time'],
+            tx['from']
+        )
+        cps_by_height[height][bhash].add(entry)
+
+        return True
+
+    return False
 
 
-existing_headers = {}
-cps_by_height = defaultdict(lambda: defaultdict(lambda: set()))
-txs_by_height = data['transactions']
+def load_data():
+    global data
+    data = {'height': cons.main_height_init, 'transactions': {}, 'blocks': {}}
+
+    try:
+        with open('data.json', 'r') as f:
+            data = json.load(f)
+
+        copyfile('data.json', 'data.backup.json')
+
+    except FileNotFoundError:
+        pass
 
 
-for hh, txs in txs_by_height.items():
-    txs.sort(key=lambda tx: (tx.get('blockNumber'), tx.get('transactionIndex')))
-    for tx in txs:
-        tx_data = tx['input']
-        data_stripped = tx_data[len(hex_prefix):]
-        height = int('0x' + data_stripped[:6], 16)
-        header = data_stripped[6:]
-        decoded_header = deserialize_header(bfh(header), height=height)
-        verified = is_verified_header(header, bits=decoded_header['bits'])
-        order = (tx['blockNumber'], tx['transactionIndex'])
-        bhash = block_hash(header)
+def save_data():
+    global data
+    with open('data.json', 'w') as f:
+        json.dump(data, f, indent=2, sort_keys=True)
 
-        print('{}:{:3} | {:7} {}'.format(order[0], order[1], height, header))
-        if not verified:
-            print('  *** Proof of work is not verified.')
-            continue
 
-        if existing_headers.get(header, None) not in (None, height):
-            print('  *** Header already exists in another height: ', existing_headers.get(header))
-            continue
+if __name__ == '__main__':
 
-        existing_headers[header] = height
+    load_data()
+    sync()
+    save_data()
 
-        print(json.dumps(decoded_header, indent=2))
-        print(bhash)
 
-        if cons.is_eligible_sub_height(height):
-            entry = CpEntry(
-                order,
-                data['blocks'][str(tx['blockNumber'])]['timestamp'],
-                decoded_header['time'],
-                tx['from']
-            )
-            cps_by_height[height][bhash].add(entry)
+    existing_headers = {}
+    cps_by_height = defaultdict(lambda: defaultdict(lambda: set()))
+    txs_by_height = data['transactions']
 
-import pprint
-pprint.pprint({
-    k: {k1: sorted((tuple(t), t.main_time - t.sub_time) for t in v1) for k1, v1 in v.items()}
-    for k, v in cps_by_height.items()
-}, width=200)
+    print('')
+
+    for hh, txs in txs_by_height.items():
+        txs.sort(key=lambda tx: (tx.get('blockNumber'), tx.get('transactionIndex')))
+        for tx in txs:
+            import_tx(tx)
+
+    print('')
+    import pprint
+    pprint.pprint({
+        k: {k1: sorted((tuple(t), t.main_time - t.sub_time) for t in v1) for k1, v1 in v.items()}
+        for k, v in sorted(cps_by_height.items(), reverse=True)
+    }, width=200)
 
